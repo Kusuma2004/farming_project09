c64a4b8dbd388ffdd1ac14d78d8a7222ddb25c2d
Kusuma2004 <135133937+Kusuma2004@users.noreply.github.com>
2025-08-30 17:48:13 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -15,7 +15,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://farming2090-3.onrender.com/api/auth/login", {
         email,
         password,
       });
